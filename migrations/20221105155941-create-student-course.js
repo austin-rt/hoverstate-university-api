64eeba53018ad120a5970737233b5d6e6c67a5e6
austin-rt <austinrtaylor91@gmail.com>
2022-11-05 12:02:51 -0400

    generate student_course model

---
+++ migrations/20221105155941-create-student-course.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students_courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students_courses');
+  },
+};
---
+++ models/studentcourse.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class StudentCourse extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  StudentCourse.init(
+    {
+      student_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'StudentCourse',
+      tableName: 'student_course',
+    }
+  );
+  return StudentCourse;
+};
