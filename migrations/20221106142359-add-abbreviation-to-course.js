fee2c374484ea8420b94f72ba44b2caaa325c0fd
austin-rt <austinrtaylor91@gmail.com>
2022-11-06 10:14:53 -0500

    remove default sequelize comments

--- migrations/20221106142359-add-abbreviation-to-course.js
+++ migrations/20221106142359-add-abbreviation-to-course.js
@@ -3,12 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.addColumn('courses', 'abbreviation', {
       type: Sequelize.STRING,
       allowNull: false,
@@ -16,12 +10,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.removeColumn('courses', 'abbreviation');
   },
 };
