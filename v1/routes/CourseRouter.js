3772d3905307bbe9c4346c57e22b8fdf56928578
austin-rt <austinrtaylor91@gmail.com>
2022-11-06 12:27:54 -0500

    remove protection from backend routes

--- v1/routes/CourseRouter.js
+++ v1/routes/CourseRouter.js
@@ -1,31 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/CourseController');
-// const middleware = require('../middleware');
 
-router.get(
-  '/',
-  // middleware.stripToken, middleware.verifyToken,
-  controller.GetAllCourses
-);
-router.get(
-  '/:id',
-  // middleware.stripToken, middleware.verifyToken,
-  controller.GetCourseById
-);
-router.post(
-  '/create',
-  // middleware.stripToken, middleware.verifyToken,
-  controller.CreateCourse
-);
-router.put(
-  '/:id',
-  // middleware.stripToken, middleware.verifyToken,
-  controller.UpdateCourseById
-);
-router.delete(
-  '/:id',
-  // middleware.stripToken, middleware.verifyToken,
-  controller.DeleteCourseById
-);
+router.get('/', controller.GetAllCourses);
+router.get('/:id', controller.GetCourseById);
+router.post('/create', controller.CreateCourse);
+router.put('/:id', controller.UpdateCourseById);
+router.delete('/:id', controller.DeleteCourseById);
 
 module.exports = router;
