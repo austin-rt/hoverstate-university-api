78cd1a15a07cf5bde98bfb54a1fa15ac00d47d45
austin-rt <austinrtaylor91@gmail.com>
2022-11-06 10:11:52 -0500

    remove defaultValue from course abbreviation

--- models/course.js
+++ models/course.js
@@ -25,7 +25,6 @@ module.exports = (sequelize, DataTypes) => {
       abbreviation: {
         type: DataTypes.STRING,
         allowNull: false,
-        defaultValue: 'CSCI 0000',
       },
     },
     {
