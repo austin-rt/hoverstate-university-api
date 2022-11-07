f788c6e92a950d6ba59c5676f620cdb6d36d6202
austin-rt <austinrtaylor91@gmail.com>
2022-11-06 22:34:39 -0500

    more courses

--- seeders/20221106140738-courses.js
+++ seeders/20221106140738-courses.js
@@ -2,54 +2,54 @@
 /** @type {import('sequelize-cli').Migration} */
 const falso = require('@ngneat/falso');
 const capitalize = (str) => {
-  return str
-    .split(' ')
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(' ');
+	return str
+		.split(' ')
+		.map((word) => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
 };
 const generateCourseCode = (course) => {
-  return course
-    .split(' ')
-    .map((word) => word[0])
-    .join('')
-    .toUpperCase();
+	return course
+		.split(' ')
+		.map((word) => word[0])
+		.join('')
+		.toUpperCase();
 };
-const introCourses = [...Array(25)].map(() => {
-  const name = falso.randSkill();
-  const courseCode = generateCourseCode(name);
-  return {
-    name: `Intro to ${capitalize(name)}`,
-    course_code: `IT${courseCode} ${falso.randNumber({
-      min: 1000,
-      max: 2999,
-    })}`,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+const introCourses = [...Array(50)].map(() => {
+	const name = falso.randSkill();
+	const courseCode = generateCourseCode(name);
+	return {
+		name: `Intro to ${capitalize(name)}`,
+		course_code: `IT${courseCode} ${falso.randNumber({
+			min: 1000,
+			max: 2999
+		})}`,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	};
 });
-const advancedCourses = [...Array(25)].map(() => {
-  const name = falso.randSkill();
-  const courseCode = generateCourseCode(name);
-  return {
-    name: `Advanced ${capitalize(name)}`,
-    course_code: `A${courseCode} ${falso.randNumber({
-      min: 3000,
-      max: 4999,
-    })}`,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+const advancedCourses = [...Array(50)].map(() => {
+	const name = falso.randSkill();
+	const courseCode = generateCourseCode(name);
+	return {
+		name: `Advanced ${capitalize(name)}`,
+		course_code: `A${courseCode} ${falso.randNumber({
+			min: 3000,
+			max: 4999
+		})}`,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	};
 });
 const courses = [...introCourses, ...advancedCourses];
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('courses', courses, {});
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert('courses', courses, {});
+	},
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('courses', null, {});
-  },
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('courses', null, {});
+	}
 };
 
 // function that capitalizes the first letter of every word of a string
