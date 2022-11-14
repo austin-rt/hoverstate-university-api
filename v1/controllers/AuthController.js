87274fbc806519ca705b1b696556f8877a44446b
austin-rt <austinrtaylor91@gmail.com>
2022-11-14 12:50:05 -0500

    add checkSession

--- v1/controllers/AuthController.js
+++ v1/controllers/AuthController.js
@@ -27,7 +27,7 @@ const Login = async (req, res) => {
 				username: user.username
 			};
 			let token = middleware.createToken(payload);
-			return res.send({ user: payload, token });
+			return res.send({ user: ...payload, token });
 		}
 		res.status(401).send({ status: 'Error', msg: 'Unauthorized' });
 	} catch (err) {
