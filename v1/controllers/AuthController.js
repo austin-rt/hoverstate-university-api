ec697570cc9d6f781c00aa8279a854bbb78e7628
austin-rt <austinrtaylor91@gmail.com>
2022-11-14 08:51:37 -0500

    add controller for auth

--- v1/controllers/AuthController.js
+++ v1/controllers/AuthController.js
@@ -2,37 +2,46 @@ const { User } = require('../../models');
 const middleware = require('../middleware');
 
 const Register = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    let passwordDigest = await middleware.hashPassword(password);
-    const user = await User.create({ username, password: passwordDigest });
-    res.send(user);
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const { username, password } = req.body;
+		let passwordDigest = await middleware.hashPassword(password);
+		const user = await User.create({ username, password: passwordDigest });
+		res.send(user);
+	} catch (err) {
+		throw err;
+	}
 };
 
 const Login = async (req, res) => {
-  try {
-    const user = await User.findOne({
-      where: { username: req.body.username },
-      raw: true,
-    });
-    if (user && middleware.comparePassword(user.password, req.body.password)) {
-      let payload = {
-        id: user.id,
-        username: user.username,
-      };
-      let token = middleware.createToken(payload);
-      return res.send({ user: payload, token });
-    }
-    res.status(401).send({ status: 'Error', msg: 'Unauthorized' });
-  } catch (err) {
-    throw err;
-  }
+	try {
+		const user = await User.findOne({
+			where: { username: req.body.username },
+			raw: true
+		});
+		if (
+			user &&
+			(await middleware.comparePassword(user.password, req.body.password))
+		) {
+			let payload = {
+				id: user.id,
+				username: user.username
+			};
+			let token = middleware.createToken(payload);
+			return res.send({ user: payload, token });
+		}
+		res.status(401).send({ status: 'Error', msg: 'Unauthorized' });
+	} catch (err) {
+		throw err;
+	}
+};
+
+const CheckSession = async (req, res) => {
+	const { payload } = res.locals;
+	res.send(payload);
 };
 
 module.exports = {
-  Register,
-  Login,
+	Register,
+	Login,
+	CheckSession
 };
--- v1/routes/AuthRouter.js
+++ v1/routes/AuthRouter.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/AuthController');
 
 router.post('/login', controller.Login);
 router.post('/register', controller.Register);
+router.get('/session', controller.CheckSession);
 
 module.exports = router;
