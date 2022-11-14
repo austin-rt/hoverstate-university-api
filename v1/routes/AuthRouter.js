9cdf04149943e7b4fcddf6e10660615d014ed0cc
austin-rt <austinrtaylor91@gmail.com>
2022-11-14 16:16:56 -0500

    remove check session artifact

--- v1/routes/AuthRouter.js
+++ v1/routes/AuthRouter.js
@@ -3,6 +3,5 @@ const controller = require('../controllers/AuthController');
 
 router.post('/login', controller.Login);
 router.post('/register', controller.Register);
-router.get('/session', controller.CheckSession);
 
 module.exports = router;
